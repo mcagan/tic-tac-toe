7c35f355ec455aed543413d013e0dd463d45dadf
Melis Cagan <meliscagan@gmail.com>
2020-06-12 14:26:28 -0400

    implement CSS

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Board from "./Board";
 
 export default function Game() {
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
+    <Fragment>
+      <h1 className="title">Tic-Tac-Toe</h1>
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
+    </Fragment>
   );
 }
